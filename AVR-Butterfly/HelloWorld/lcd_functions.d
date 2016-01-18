baa8c2ff589766f03816eb854b3958cbdf499bf2
Kiral Poon <kiralpoon@gmail.com>
2016-01-19 04:14:48 +0800

    Put helloworld with instructions

---
+++ AVR-Butterfly/HelloWorld/lcd_functions.d
@@ -0,0 +1,2 @@
+lcd_functions.o lcd_functions.d : lcd_functions.c lcd_driver.h lcd_functions.h bcd.h \
+ main.h
---
+++ AVR-Butterfly/HelloWorld/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// just in case boolean constants aren't defined anywhere else
+#ifndef FALSE
+#  define FALSE (0)
+#endif
+#ifndef TRUE
+#  define TRUE (!FALSE)
+#endif
+// other stuff goes here (if needed)
+
+#endif
